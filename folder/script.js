84506baf44d721f352678640716226c3bdef0eba
Keith McHugh <mchughkeith3@gmail.com>
2022-02-08 11:33:53 -0500

    chidori

--- folder/script.js
+++ folder/script.js
@@ -1,7 +1,10 @@
-function pain(){
-  audioObj = new Audio(Almightypush.mp3);
-myAudioElement.addEventListener("canplaythrough", event => {
+function AudioPlay(name = new String()) {
+  audioObj = new Audio(name);
+  audioObj.id = "playingAudio";
+  $audiobtn = document.getElementById("audiobtn");
+  audioObj.addEventListener("canplaythrough", function(event) {
     /* the audio is now playable; play it if permissions allow */
-    myAudioElement.play();
+    this.play();
   });
+  $audiobtn.onclick = function() {};
 }
---
+++ folder/script3.js
@@ -0,0 +1,10 @@
+function AudioPlay(name = new String()) {
+    audioObj = new Audio(name);
+    audioObj.id = "playingAudio";
+    $audiobtn = document.getElementById("audiobtn");
+    audioObj.addEventListener("canplaythrough", function(event) {
+      /* the audio is now playable; play it if permissions allow */
+      this.play();
+    });
+    $audiobtn.onclick = function() {};
+}
